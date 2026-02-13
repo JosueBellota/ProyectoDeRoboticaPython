10bb03393d6334ba8300366a83d6da7d937dd0ba
Josue Bellota Ichaso <69655184+JosueBellota@users.noreply.github.com>
2026-02-13 22:06:51 +0100

    Entregable

---
+++ Collabs/Python/05Entregable/Aircraft.py
@@ -0,0 +1,22 @@
+class Aircraft:
+    """ Creamos una nueva persona con un nombre y una edad """
+    unidad_masa = "kg" # Atributo de clase
+
+    def __init__(self, n, e):
+        self.nombre = n
+        self.edad = e
+
+    def seating_plan():
+        """Generates a seating plan for the number of rows and seats per row
+        Returns:
+        rows: A list of Nones (size num_rows + 1).
+        seats: A string of letters such as "ABCDEF"
+        """
+    def num_seats():
+        """Calculates the number of seats
+        Returns:
+        seats: The number of seats
+        """
+
+    def mostrar(self):
+        print("El nombre es ", self.nombre, " y la edad es ", self.edad)
