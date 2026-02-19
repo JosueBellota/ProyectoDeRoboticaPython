69e3e40d844b90ca3ace496b927b7ba53c9ec7c5
Josue Bellota Ichaso <69655184+JosueBellota@users.noreply.github.com>
2026-02-19 18:32:38 +0100

    clase aircraft terminado

--- Collabs/Python/05Entregable/src/Aircraft.py
+++ Collabs/Python/05Entregable/src/Aircraft.py
@@ -18,40 +18,68 @@ class Aircraft:
 
     def __init__(self, registration, model, num_rows, num_seats_per_row):
 
-        self.registration = registration
-        self.model  = model
-        self.num_rows = num_rows
-        self.num_seats_per_row = num_seats_per_row
+        # supera el limite del abecedario 
+        if(num_seats_per_row > 26):
+            
+            print("the numbers of seat per row cannot be more than 26 ")
+        else:
+            self.__registration = registration
+            self.__model  = model
+            self.__num_rows = num_rows
+            self.__num_seats_per_row = num_seats_per_row
 
-    def get_registration():
+    def get_registration(self):
+
+        """Calculates the number of seats
+        Returns:
+        seats: The number of seats
+        """
+
+        return(self.__registration)
+
+    def get_model(self):
 
         """Calculates the number of seats
         Returns:
         seats: The number of seats
         """
 
-        return __registration
-    def get_model():
-
-        """Calculates the number of seats
-        Returns:
-        seats: The number of seats
-        """
+        return(self.__model)
         
 
 
-    def seating_plan():
+    def seating_plan(self):
         """Generates a seating plan for the number of rows and seats per row
         Returns:
         rows: A list of Nones (size num_rows + 1).
         seats: A string of letters such as "ABCDEF"
         """
-    def num_seats():
+
+        caracteres = []
+        # alfabeto ingles
+        # Mayúsculas	A	65
+        # Minúsculas	a	97
+        for i in range(65, 65 + self.__num_seats_per_row):
+
+            caracteres.append(chr(i))
+        
+        letters = "".join(caracteres)
+
+        return(self.__num_rows, letters)
+            
+
+       
+    def num_seats(self):
         """Calculates the number of seats
         Returns:
         seats: The number of seats
         """
+        
+        number_of_seats = self.__num_rows * self.__num_seats_per_row
 
 
-    def mostrar(self):
-        print("El nombre es ", self.nombre, " y la edad es ", self.edad)
+        return(number_of_seats)
+
+
+
+
--- Collabs/Python/05Entregable/src/test.py
+++ Collabs/Python/05Entregable/src/test.py
@@ -1,10 +1,26 @@
 from Passenger import Passenger
 
+from Aircraft import Aircraft
+
 def main():
 
-    p1 = Passenger("Jack", "Shephard", "85994003S")
+    # [ ] p1 = Passenger("Jack", "Shephard", "85994003S")
 
+    a1 = Aircraft(registration = "G-EUAH",
+                  model = "Airbus A319",
+                  num_rows = 22, 
+                  num_seats_per_row=6)
+    
+    a2 = Aircraft(registration = "G-EUAH",
+                model = "Airbus A319",
+                num_rows = 22, 
+                num_seats_per_row=30)
+    
 
-    print(p1.passenger_data())
+    print(a1.get_registration())
+    print(a1.get_model())
+    print(a1.seating_plan())
+    print(a1.num_seats())
+    # print(p1.passenger_data())
 
 main()
